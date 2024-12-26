bb87c3fd4a2369314b0b873791e533727fbdfaba
Muhammad Farraos Abdillah <zqr.farraosohaada@gmail.com>
2024-12-26 20:26:06 +0700

    first commit

--- js/layup-drawer.js
+++ js/layup-drawer.js
@@ -1,55 +1,33 @@
 'use strict';
 
 function LayupDrawer() {
+    /**
+     * Canvas element
+     */
     this.canvas = null;
-    this.ctx = null;
-    this.images = {
-        parallel: null,
-        perpendicular: null,
-    };
 }
 
 LayupDrawer.prototype = {
     /**
-     * Inisialisasi canvas dan memuat gambar
-     * @param {HTMLCanvasElement} canvas
+     * Configure the canvas
+     *
+     * @param {HTMLCanvasElement} canvas  Canvas element
      */
-    init: function (canvas) {
-        this.canvas = canvas;
-        this.ctx = canvas.getContext('2d');
-
-        // Memuat gambar
-        this.images.parallel = new Image();
-        this.images.parallel.src = './2D/javascript-test/images/paralel-grain-0.jpg';
-
-        this.images.perpendicular = new Image();
-        this.images.perpendicular.src = './2D/javascript-test/images/perpendicular-grain-90.jpg';
+    init : function (canvas) {
+        
     },
 
     /**
-     * Gambar pola lapisan CLT
-     * @param {Array} pattern
+     * Draw a layup configuration on the canvas
+     *
+     * @param {Object} layup Layup object structure
+     * @param {Number} length Layup length in mm
      */
-    drawPattern: function (pattern) {
-        const ctx = this.ctx;
-        const canvasWidth = this.canvas.width;
-        const canvasHeight = this.canvas.height;
+    drawLayup : function (layup, length) {
 
-        let currentY = 0; // Posisi vertikal saat ini di canvas
-
-        // Tunggu gambar selesai dimuat
-        this.images.parallel.onload = this.images.perpendicular.onload = () => {
-            const layerHeight = canvasHeight / pattern.length; // Tinggi tiap lapisan
-
-            pattern.forEach((layer) => {
-                const img = layer.type === 'parallel' ? this.images.parallel : this.images.perpendicular;
-
-                for (let i = 0; i < layer.count; i++) {
-                    ctx.drawImage(img, 0, currentY, canvasWidth, layerHeight);
-                }
-
-                currentY += layerHeight;
-            });
-        };
     },
+
+    /**
+     * Add more functions as you need
+     */
 };
